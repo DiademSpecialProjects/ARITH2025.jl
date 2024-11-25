84841e4d1fec4d9ae39c6fa59737784c5505ff58
Jeffrey Sarnoff <jeffrey.sarnoff@gmail.com>
2024-11-25 18:27:14 -0500

    combinatorics

--- src/ARITH2025.jl
+++ src/ARITH2025.jl
@@ -1,5 +1,10 @@
 module ARITH2025
 
+export all_unique_pairs, all_unique_triples, 
+       all_pairs_commutative, all_triples_firsttwo_commutative
+
 using Combinatorics
+
+include("combinatorics.jl")
 
 end # module
